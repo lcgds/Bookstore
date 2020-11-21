3f74fed72b932ee95cc727331e581446044a2e55
LCGDS <lcgds@live.com>
2020-11-21 11:39:55 -0300

    Update

--- php/index.php
+++ php/index.php
@@ -17,8 +17,34 @@
 
         <?php require_once './require/header.php';?>
 
-        <main>
-            Sobre
+        <main class="container my-5">
+            
+        <div class="card bg-light">
+            <div class="row no-gutters">
+
+                <div class="col">
+                    <img class="card-img" src="../img/shunya-koide-1emWndlDHs0-unsplash.jpg">
+                </div>
+                <div class="col-7">
+                    <div class="card-body">
+                        <h2 class="card-title">Sobre nós</h2>
+                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea dolorem optio eligendi minus voluptate sint velit, consequuntur vel animi, ab explicabo cum. Beatae aperiam facere corporis alias, voluptate impedit et?</p>
+                        <h3 class="h4 card-title">Missão</h3>
+                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        <h3 class="h4 card-title">Visão</h3>
+                        <p class="card-text">At, sapiente rem explicabo laboriosam obcaecati praesentium laborum temporibus cum illo necessitatibus fuga cumque tempore.</p>
+                        <h3 class="h4 card-title">Valores</h3>
+                        <p class="card-text">Rerum sequi iusto delectus minus, fugiat molestias.</p>
+                    </div>
+                </div>
+
+            </div>
+
+            
+
+
+        </div>
+
         </main>
 
         <?php require_once './require/footer.php';?>
