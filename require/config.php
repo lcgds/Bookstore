faec042d606c585ed0a98e0b1eb813c3ddf43722
LCGDS <lcgds@live.com>
2020-11-28 11:37:55 -0300

    Update

--- require/config.php
+++ require/config.php
@@ -1,22 +1,6 @@
 <?php
 
 //Credenciais de acesso ao banco de dados
-<<<<<<< HEAD
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'admin');
-define('DB_PASSWORD', 'Eunaosei!123@');
-define('DB_NAME', 'LivrariaTSI');
-
-//Conexão com banco de dados
-$dblink = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
-
-//Checagem de conexão com banco de dados
-if ($dblink === false) {
-    die("Erro de conexão com banco de dados. " . mysqli_connec_error());
-}
-
-?>
-=======
 // define('DB_SERVER', 'localhost');
 // define('DB_USERNAME', 'admin');
 // define('DB_PASSWORD', 'Eunaosei!123@');
@@ -45,4 +29,3 @@ catch(PDOException $exc){
 catch(Exception $exc){
     echo 'Erro genérico: '.$exc->getMessage();
 }
->>>>>>> renan
